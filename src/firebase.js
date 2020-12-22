0f22c89e028762565848908277c013d02d06f6fd
yarinek <mareks726@gmail.com>
2020-12-22 13:39:30 +0100

    Added Amazon header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDbBM6I_wPdF6Myso6r3g99Pcxf4tSJ_Ss",
+    authDomain: "clone-5ac71.firebaseapp.com",
+    projectId: "clone-5ac71",
+    storageBucket: "clone-5ac71.appspot.com",
+    messagingSenderId: "175544333296",
+    appId: "1:175544333296:web:aaab3cc84a8aaab5f9a335",
+    measurementId: "G-NHL0QER84S"
+};
