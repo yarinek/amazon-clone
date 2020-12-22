849a59232cc6b10bcbab0c21f87fb59fedd02992
yarinek <mareks726@gmail.com>
2020-12-22 21:05:06 +0100

    Added checkout view + number of items in basket

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home'
+import Checkout from './Checkout'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -8,13 +9,12 @@ function App() {
     // BEM
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/checkout">
-            <Header />
-            <h1>I AM THE CHECKOUT</h1>
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import "./Product.css"
+import { useStateValue } from './StateProvider'
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    console.log("this is the basket >>>>", basket)
+
+    const addToBasket = () => {
+        //dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    }
     return (
         <div className="product">
             <div className="product__info">
@@ -21,7 +39,7 @@ function Product({ title, image, price, rating }) {
 
             <img src={image} alt="" />
 
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
 
         </div>
     )
